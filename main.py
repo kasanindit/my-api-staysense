4f0e6099a98c7be739e98f8aa8a7dd94a1c64552
kasanindit <kasanindit@gmail.com>
2025-05-10 22:18:28 +0700

    fix: endpoint 'churn' and set doc to not case sensitive

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-import awsgi
 from flask import Flask, request, jsonify
 import pandas as pd
 import numpy as np
 import os
 import joblib
-import json
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 from datetime import datetime
 
-
-firebase_credentials = json.loads(os.getenv('FIREBASE_CREDENTIALS'))
-cred = credentials.Certificate(firebase_credentials)
-
+cred = credentials.Certificate("staysenseKey.json")
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'staysense-624b4.firebasestorage.app'
 })
-
-
 
 db = firestore.client() 
 
 app = Flask(__name__)
 
-# Load the model using pickle
-model_path = os.path.join("model", "model_TabNet3.pkl")
+model_path = os.path.join("model", "tabnet_model.pkl")
 model = joblib.load(model_path)
 
-# Kolom yang dibutuhkan
-required_cols = [
-    "Age", "Number_of_Dependents", "City", "Tenure_in_Months",
-    "Internet_Service", "Online_Security", "Online_Backup", "Device_Protection_Plan",
-    "Premium_Tech_Support", "Streaming_TV", "Streaming_Movies", "Streaming_Music",
-    "Unlimited_Data", "Contract", "Payment_Method", "Monthly_Charge",
-    "Total_Charges", "Total_Revenue", "Satisfaction_Score", "Churn_Score", "CLTV"
-]
 
+# Kolom yang dibutuhkan
+# required_cols = [
+#     "Age", "Number_of_Dependents", "City", "Tenure_in_Months",
+#     "Internet_Service", "Online_Security", "Online_Backup", "Device_Protection_Plan",
+#     "Premium_Tech_Support", "Streaming_TV", "Streaming_Movies", "Streaming_Music",
+#     "Unlimited_Data", "Contract", "Payment_Method", "Monthly_Charge",
+#     "Total_Charges", "Total_Revenue", "Satisfaction_Score", "Churn_Score", "CLTV"
+# ]
+
+required_cols = [
+    "age", "number_of_dependents", "city", "tenure_in_months",
+    "internet_service", "online_security", "online_backup", "device_protection_plan",
+    "premium_tech_support", "streaming_tv", "streaming_movies", "streaming_music",
+    "unlimited_data", "contract", "payment_method", "monthly_charge",
+    "total_charges", "total_revenue", "satisfaction_score", "churn_score", "cltv"
+]
 
 
 @app.route("/", methods=["GET"])
@@ -46,7 +46,7 @@ def index():
 def predict():
     try:
         data = request.get_json()
-
+        
         input_data = np.array([[ 
             data["age"],
             data["number_of_dependents"],
@@ -73,16 +73,17 @@ def predict():
 
         churn_probability = model.predict_proba(input_data)[0][1]
 
+        # output yang keluar
         if churn_probability > 0.5:
             result = {
                 "is_churn": True,
-                "churn_probability": f"{round(churn_probability * 100, 2):.2f}%",
+                "churn_rate": f"{round(churn_probability * 100, 2):.2f}%",
                 "message": f"Customer will churn with probability {churn_probability * 100:.2f}%"
             }
         else:
             result = {
                 "is_churn": False,
-                "churn_probability": f"{round((1 - churn_probability) * 100, 2):.2f}%",
+                "not_churn_rate": f"{round((1 - churn_probability) * 100, 2):.2f}%",
                 "message": f"Customer will not churn with probability {(1 - churn_probability) * 100:.2f}%"
             }
 
@@ -90,19 +91,33 @@ def predict():
         now = datetime.now()
         month_str = now.strftime("%Y-%m")
 
+        # output masuk ke firestore
         db.collection("predictions").add({
+            "input_source":"manual",
             "timestamp": now.isoformat(),
             "month": month_str,
             "is_churn": result["is_churn"],
-            "churn_probability": float(churn_probability),
-            "customer_data": data  
+            "rate": float(churn_probability),
+            "customer_data": data 
         })
-            
-        return jsonify({"prediction": result})
+        
+        return jsonify({
+            "status": "success",
+            "prediction": result
+        })
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
+        return jsonify({
+            "status": "error",
+            "message": f"Missing input data : {str(e)}",
+            "prediction": {
+                "is_churn": "unknown",
+                "churn_probability": "unknown",
+                "message": "Prediction failed due to an internal error."
+            }
+        }), 500
+            
+    
 
 def upload_to_storage(file, filename, folder="uploaded_files"):
     bucket = storage.bucket()
@@ -125,12 +140,16 @@ def upload():
         df = pd.read_excel(file)
     else:
         return jsonify({"error": "Unsupported file format. Only CSV, XLS, XLSX allowed."}), 400
+    
+    # cek nama kolom
+    df.columns = df.columns.str.lower().str.replace(' ', '_').str.replace('[^a-zA-Z0-9_]', '', regex=True)
 
     missing_cols = [col for col in required_cols if col not in df.columns]
     if missing_cols:
         return jsonify({"error": f"Missing columns: {missing_cols}"}), 400
 
     try:
+        
         # Prediksi
         df = df[required_cols]
         input_data = df.to_numpy()
@@ -143,7 +162,9 @@ def upload():
         file.stream.seek(0) 
         file_url = upload_to_storage(file, filename)
         
+        # summary adalah output yang akan ditampilkan 
         summary = {
+            "input_source": "Upload file",
             "total_customers": total_customers,
             "churn_count": churn_count,
             "not_churn_count": total_customers - churn_count,
@@ -154,14 +175,21 @@ def upload():
             "month": datetime.now().strftime("%Y-%m")
         }
 
+        # dan akan terkirim ke firestore
         db.collection("predictions").add(summary)
-
-        return jsonify({"summary": summary})
+        
+        return jsonify({
+            "status": "success",
+            "summary": summary
+        })
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
-    
+        return jsonify({
+            "status": "error",
+            "message": str(e)
+        }), 500
 
+    
 @app.route("/history", methods=["GET"])
 def get_summary_history():
     try:
@@ -182,9 +210,11 @@ def get_chart_data():
 
         for doc in docs:
             data = doc.to_dict()
-            is_churn = data["is_churn"]
-            month = data["month"]
+            is_churn = data.get("is_churn", False)
+            month = data.get("month", "")
 
+            if not month:
+                continue 
             if is_churn:
                 total_churn += 1
             else:
@@ -192,14 +222,14 @@ def get_chart_data():
 
             if month not in per_month:
                 per_month[month] = {"churn": 0, "total": 0}
-            per_month[month]["total"] += 1
+            per_month[month]["total"] += 1 
             if is_churn:
-                per_month[month]["churn"] += 1
+                per_month[month]["churn"] += 1 
 
         churn_rate_per_month = [
             {
                 "month": m,
-                "churn_rate": round((d["churn"] / d["total"]) * 100, 2)
+                "churn_rate": round((d["churn"] / d["total"]) * 100, 2) if d["total"] > 0 else 0
             }
             for m, d in sorted(per_month.items())
         ]
@@ -214,6 +244,7 @@ def get_chart_data():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 
 if __name__ == "__main__":
